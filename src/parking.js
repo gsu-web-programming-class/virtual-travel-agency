4a0ff92eb29ca0c0b53e37f9d254542abc518cc8
hamelpatel <hpatel83@student.gsu.edu>
2017-12-03 17:56:55 -0500

    parking

---
+++ src/parking.js
@@ -0,0 +1,108 @@
+function myFunction1() {
+    document.getElementById("button1").disabled = true;
+}
+function myFunction2() {
+    document.getElementById("button2").disabled = true;
+}
+function myFunction3() {
+    document.getElementById("button3").disabled = true;
+}
+function myFunction4() {
+    document.getElementById("button4").disabled = true;
+}
+function myFunction5() {
+    document.getElementById("button5").disabled = true;
+}
+function myFunction6() {
+    document.getElementById("button6").disabled = true;
+}
+function myFunction7() {
+    document.getElementById("button7").disabled = true;
+}
+function myFunction8() {
+    document.getElementById("button8").disabled = true;
+}
+function myFunction9() {
+    document.getElementById("button9").disabled = true;
+}
+function myFunction10() {
+    document.getElementById("button10").disabled = true;
+}
+function myFunction11() {
+    document.getElementById("button11").disabled = true;
+}
+function myFunction12() {
+    document.getElementById("button12").disabled = true;
+}
+function myFunction13() {
+    document.getElementById("button13").disabled = true;
+}
+function myFunction14() {
+    document.getElementById("button14").disabled = true;
+}
+function myFunction15() {
+    document.getElementById("button15").disabled = true;
+}
+function myFunction16() {
+    document.getElementById("button16").disabled = true;
+}
+function myFunction17() {
+    document.getElementById("button17").disabled = true;
+}
+function myFunction18() {
+    document.getElementById("button18").disabled = true;
+}
+function myFunction19() {
+    document.getElementById("button19").disabled = true;
+}
+function myFunction20() {
+    document.getElementById("button20").disabled = true;
+}
+function myFunction21() {
+    document.getElementById("button21").disabled = true;
+}
+function myFunction22() {
+    document.getElementById("button22").disabled = true;
+}
+function myFunction23() {
+    document.getElementById("button23").disabled = true;
+}
+function myFunction24() {
+    document.getElementById("button24").disabled = true;
+}
+function myFunction25() {
+    document.getElementById("button25").disabled = true;
+}
+function myFunction26() {
+    document.getElementById("button26").disabled = true;
+}
+function myFunction27() {
+    document.getElementById("button27").disabled = true;
+}
+function myFunction28() {
+    document.getElementById("button28").disabled = true;
+}
+function myFunction29() {
+    document.getElementById("button29").disabled = true;
+}
+function myFunction30() {
+    document.getElementById("button30").disabled = true;
+}
+function myFunction31() {
+    document.getElementById("button31").disabled = true;
+}
+function myFunction32() {
+    document.getElementById("button32").disabled = true;
+}
+function myFunction33() {
+    document.getElementById("button33").disabled = true;
+}
+function myFunction34() {
+    document.getElementById("button34").disabled = true;
+}
+function myFunction35() {
+    document.getElementById("button35").disabled = true;
+}
+function myFunction36() {
+    document.getElementById("button36").disabled = true;
+}
