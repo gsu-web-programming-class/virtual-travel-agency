fffc2bea20f4a5c4d43c7af0487fe0aa2edd3e3c
Joshua King <joshua@antechdevelopment.com>
2017-12-04 23:34:27 -0500

    Added footer

--- src/includes/main_layout_bottom.php
+++ src/includes/main_layout_bottom.php
@@ -1,3 +1,13 @@
 </main>
+<footer class="mdl-mini-footer">
+    <div class="mdl-mini-footer__left-section">
+        <div class="mdl-logo">Virtual Travel Agency</div>
+        <ul class="mdl-mini-footer__link-list">
+            <li><a href="/cart">View Cart</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/logout">Locgout</a></li>
+        </ul>
+    </div>
+</footer>
 </div>
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
