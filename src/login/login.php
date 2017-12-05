03eeec76738f5eb0301800fba09d8cce634b2bfe
joanaibarra <joana97.ji@gmail.com>
2017-12-05 04:38:50 -0500

    Add session values, not sure about their values

--- src/login/login.php
+++ src/login/login.php
@@ -1,4 +1,11 @@
 <?php
     session_start();
 
+    $username = $_POST['user1'];
+    $password = $_POST['password1'];
+
     $_SESSION[ "user_id" ] = "3";
+    $_SESSION[ "username" ] = $username;
+    $_SESSION[ "password" ] = $password;
+
+?>
