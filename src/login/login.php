619d53f4487fb95bd917cbe505d61d90a95bc217
joanaibarra <joana97.ji@gmail.com>
2017-12-05 04:51:03 -0500

    Affect db

--- src/login/login.php
+++ src/login/login.php
@@ -8,4 +8,14 @@
     $_SESSION[ "username" ] = $username;
     $_SESSION[ "password" ] = $password;
 
+    //establish a connection to our db
+    //$connection = mysql_connect("localhost", "root", "");
+    //$db = mysql_select_db("our_db", $connection);
+
+    //if (isset($_POST['name1])){
+    //  $query = mysql_query("insert into TABLE_NAME(user_id, username, password,... any other fields)
+    //           values ('$username', '$password', ...)");
+    //  echo "form submitted successfully";
+    //}
+    //mysql_close($connection);
 ?>
