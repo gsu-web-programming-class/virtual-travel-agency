f58c8d90ef25134571102b06483a162bad021b09
hamelpatel <hpatel83@student.gsu.edu>
2017-12-05 02:51:49 -0500

    parking

---
+++ src/parkdb.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    class parkDB
+       {
+           private $conn;
+
+           public function __construct ()
+           {
+               $this->connect();
+           }
+
+           private function connect ()
+           {
+               $DB_HOST     = "virtualtravelagency_db_1";
+               $DB_USER     = "virtualtravelagency";
+               $DB_PASSWORD = "virtualtravelagency";
+               $DB_DB       = "virtualtravelagency";
+
+               $this->conn = mysqli_connect( $DB_HOST, $DB_USER, $DB_PASSWORD )
+               or die ( "<br/>Could not connect to MySQL server" );
+
+               mysqli_select_db( $this->conn, $DB_DB )
+               or die ( "<br/>Could not select the indicated database" );
+           }
+
+  $parkdb = new parkDB();
---
+++ src/parker.php
@@ -0,0 +1,64 @@
+<?php
+
+    class parker
+    {
+        private $month;
+        private $day;
+        private $year;
+        private $price;
+        private $spot;
+
+
+        public function __construct ( $month, $day, $year, $price, $spots)
+        {
+            $this->month = $month;
+            $this->day  = $day;
+            $this->year  = $year;
+            $this->price  = $price;
+            $this->spot   = $spot;
+        }
+
+        /** @noinspection PhpUndefinedFieldInspection */
+        public static function from_query_result ( parkerQueryResult $parker_user )
+        {
+            $month  = $parker_user->month;
+            $day    = $parker_user->day;
+            $year   = $parker_user->year;
+            $price  = $parker_user->price;
+            $spot   = $parker_user->spot;
+            return new parker( $month, $day, $year, $price, $spot );
+        }
+
+
+        public function getmonth ()
+        {
+            return $this->month;
+        }
+
+        public function getday ()
+        {
+            return $this->day;
+        }
+
+        public function getyear ()
+        {
+            return $this->year;
+        }
+
+        public function getprice ()
+        {
+            return $this->price;
+        }
+
+        public function getspot ()
+        {
+            return $this->spor;
+        }
+
+        public function setspot ( $spot )
+        {
+            $this->spot = $spot;
+        }
+
+    }
+?>
